2aa585c579efa39d6d642c663cd52501a82da13b
Brandon Reiser <reiserb58@gmail.com>
2023-09-17 19:44:58 -0500

    9/17 commit

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,11 +1,10 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
-
 // TODO 1.1a: Define candidateName // 
+
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first american woman in space? ";
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
@@ -38,14 +37,8 @@ function gradeQuiz(candidateAnswers) {
       console.log("You better go study up pal.")
   }
 
-  let grade;
-    if (isCorrect == true) {
-      grade = 100;
-  } else {
-  grade = 0;
-  }
+  
 
-  return grade;
 }
 
 function runProgram() {
