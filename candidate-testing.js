4bdbedb53384faed5c06c1e68c1029155fec0ad7
Brandon Reiser <reiserb58@gmail.com>
2023-09-26 20:31:16 -0500

    commited 9.26

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,13 +10,13 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space", "True or false: 5 kilometer == 5000 meters", "(5+3)/2 * 10 = ?", "Given the array \\[8 , 'Orbit', 'Trajectory', rt , what entry is at index 2 \\]", "What is the minimum crew size for the ISS"];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-    candidateName = input.question("Please enter your name")
+    candidateName = input.question("What is your name?")
 }
 
 function askQuestion() {
@@ -31,22 +31,27 @@ function askQuestion() {
 
 function gradeQuiz(candidateAnswers) {
 
+
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  
-  for (let j = 0; j < candidateAnswer.length; j++) {
-    console.log("My answer is ${candidateAnswer[0]}"); }
-    for (let O = 0; O < correctAnswers.length; O++) {
-      console.log("The correct answer is ${candidateAnswers[0]}"); }
-    
-
-  
-
+  let percentageCorrect = 0;
+  for (let J = 0; J < candidateAnswers.length; J++) {
+    console.log(`${J + 1}) ${questions[J]}`)
+    console.log(`My answer is ${candidateAnswers[J]}`); 
+    console.log(`The correct answer is ${correctAnswers[J]}\n`); 
+    if (candidateAnswers[J].toLowerCase() === correctAnswers[J].toLowerCase()){
+      percentageCorrect = percentageCorrect + 1; 
+    }
+  }
+  console.log(`Students graded percent is ${(percentageCorrect / candidateAnswers.length * 100)}`)
+  if ((percentageCorrect / candidateAnswers.length * 100) < 60){
+    console.log("Status failed")
+  }
+  return (percentageCorrect / candidateAnswers.length * 100);
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello, " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
