ec5515969425f87feb38a4c866abf937ab14e900
Brandon Reiser <reiserb58@gmail.com>
2023-09-28 19:37:49 -0500

    final revision 9.28

--- candidate-testing.js
+++ candidate-testing.js
@@ -52,6 +52,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
+  console.log(Welcome + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
